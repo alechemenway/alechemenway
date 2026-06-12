cfe99925b037b4d951041286efc09162182b9fb5
alechemenway <hemenway.alec@gmail.com>
2026-06-12 08:34:11 -0500

    Swap homepage carousel photo order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -246,7 +246,7 @@ function Photos() {
   return (
     <div className="mt-16 sm:mt-20">
       <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
-        {[image1, image2, image3, image4, image5].map((image, imageIndex) => (
+        {[image5, image2, image3, image4, image1].map((image, imageIndex) => (
           <div
             key={image.src}
             className={clsx(
